541fd252b48cc808433f96fd7d3dfafc23239c4e
joblo <joblo@JONAS-PC>
2019-01-05 21:58:35 +0100

    Java Test

--- src/TimedAnimationTest.java
+++ src/TimedAnimationTest.java
@@ -1,60 +1,54 @@
 
 public class TimedAnimationTest {
 
-	public boolean gameRunning = true;
-	private long lastFpsTime;
-	private int fps;
+	private boolean appRunning = true;
+	private long lastFpsTime = 0;
+	private int fps = 0;
+	
+	public static void main(String[] args) {
+		TimedAnimationTest tat = new TimedAnimationTest();
+		tat.mainLoop();
+	}
 
-	public void gameLoop() {
+	public void mainLoop() {
 		long lastLoopTime = System.nanoTime();
 		final int TARGET_FPS = 60;
 		final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
 
-		// keep looping round til the game ends
-		while (gameRunning) {
-			// work out how long its been since the last update, this
-			// will be used to calculate how far the entities should
-			// move this loop
+		while (appRunning) {
 			long now = System.nanoTime();
 			long updateLength = now - lastLoopTime;
 			lastLoopTime = now;
 			double delta = updateLength / ((double) OPTIMAL_TIME);
 
-			// update the frame counter
 			lastFpsTime += updateLength;
 			fps++;
 
-			// update our FPS counter if a second has passed since
-			// we last recorded
 			if (lastFpsTime >= 1000000000) {
 				System.out.println("(FPS: " + fps + ")");
 				lastFpsTime = 0;
 				fps = 0;
 			}
 
-			// update the game logic
-			doGameUpdates(delta);
+			doAnimationUpdates(delta);
 
-			// draw everyting
 			render();
 
-			// we want each frame to take 10 milliseconds, to do this
-			// we've recorded when we started the frame. We add 10 milliseconds
-			// to this and then factor in the current time to give
-			// us our final value to wait for
-			// remember this is in ms, whereas our lastLoopTime etc. vars are in ns.
-//	      try {
-//	    	  Thread.sleep( (lastLoopTime-System.nanoTime() + OPTIMAL_TIME)/1000000 )
-//	    	  };
+	    	try {
+				Thread.sleep( (lastLoopTime-System.nanoTime() + OPTIMAL_TIME)/1000000 );
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 
 	private void render() {
 		// TODO Auto-generated method stub
-
 	}
 
-	private void doGameUpdates(double delta) {
+	private void doAnimationUpdates(double delta) {
 //		for (int i = 0; i < stuff.size(); i++) {
 //			// all time-related values must be multiplied by delta!
 //			Stuff s = stuff.get(i);
